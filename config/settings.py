ac2b678cf7619a0f94c16410878a74d5539f019c
Mahi Islam Barshan <59387836+barshon-ex@users.noreply.github.com>
2026-06-29 12:00:39 +0600

    Add environment variable loading to settings

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+WORDPRESS_URL = os.getenv("WORDPRESS_URL")
+
+WORDPRESS_USERNAME = os.getenv("WORDPRESS_USERNAME")
+
+WORDPRESS_APPLICATION_PASSWORD = os.getenv("WORDPRESS_APPLICATION_PASSWORD")
+
+POST_STATUS = os.getenv("POST_STATUS", "draft")
