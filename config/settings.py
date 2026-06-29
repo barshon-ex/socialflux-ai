1f9d22a4fac4067c75c34ab85a5fe995a9653310
Mahi Islam Barshan <59387836+barshon-ex@users.noreply.github.com>
2026-06-29 13:07:58 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# AI
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+# WordPress
 WORDPRESS_URL = os.getenv("WORDPRESS_URL")
-
 WORDPRESS_USERNAME = os.getenv("WORDPRESS_USERNAME")
-
 WORDPRESS_APPLICATION_PASSWORD = os.getenv("WORDPRESS_APPLICATION_PASSWORD")
 
+# Post Settings
 POST_STATUS = os.getenv("POST_STATUS", "draft")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
